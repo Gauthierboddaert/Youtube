8e34c6ea889f9d8b204869494836aa7a3903f8e8
rayles59 <83212748+rayles59@users.noreply.github.com>
2022-02-12 11:27:36 +0100

    Ajoutez des fichiers projet.

---
+++ Youtube/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Youtube
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dlg = new VistaFolderBrowserDialog();
+            //verify if window is open
+            if (dlg.ShowDialog() == true)
+            {
+                //Select Path
+                var path =  dlg.SelectedPath;
+
+                // Open and write in terminal
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = ""; // Commande à exécuter
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+            }
+        }
+    }
+}
